9a4456eb785de859655cf98b487753d03bb0656d
Gerard Forcada <gerardforcada@gmail.com>
2021-04-10 03:17:25 +0200

    Form, Validation, Save data and Log in

---
+++ cxrlogin.php
@@ -0,0 +1,149 @@
+<?php
+/*
+Plugin Name: CxR | Identitat Digital Republicana
+Plugin URI: https://siriondev.com
+description: Integració amb el procés de validació de la Identitat Digital Republicana del Consell per la República Catalana
+Version: 1.0
+Author: Sirion Developers
+Author URI: https://siriondev.com
+License: GPL2
+*/
+
+add_action('user_new_form', "show_cxr_idrepublicana" );
+add_action('edit_user_profile', 'show_cxr_idrepublicana');
+add_action('show_user_profile', 'show_cxr_idrepublicana');
+
+add_action('user_register', 'save_cxr_idrepublicana');
+add_action('profile_update', 'save_cxr_idrepublicana');
+add_action('profile_update', 'save_cxr_idrepublicana', 10, 2);
+
+add_action('user_profile_update_errors', 'validate_cxr_idrepublicana', 0, 3);
+add_action('wp_authenticate', 'authenticate_cxr_idrepublicana');
+
+/**
+ * Mostra el formulari per a introduir la Identitat Digital Republicana
+ *
+ * @param User $user
+ *
+ * @return void
+ */
+function show_cxr_idrepublicana($user)
+{
+    $meta = $users = get_user_meta($user->id);
+
+    $cxr_idrepublicana = isset($meta['cxr_idrepublicana'][0]) ? $meta['cxr_idrepublicana'][0] : ''; ?>
+
+    <h3 class="heading">Consell per la República</h3>
+
+    <table class="form-table">
+
+        <tr>
+
+            <th><label for="contact">Identitat Digital Republicana</label></th>
+
+            <td><input type="text" class="input-text form-control" name="cxr_idrepublicana" id="cxr_idrepublicana" placeholder="C-999-99999" value="<?php echo $cxr_idrepublicana; ?>"/></td>
+
+        </tr>
+
+    </table> <?php
+}
+
+
+/**
+ * Guarda el valor de la Identitat Digital Republicana
+ *
+ * @param int $user_id
+ * @param array $old_user_data
+ *
+ * @return void
+ */
+function save_cxr_idrepublicana($user_id, $old_user_data)
+{
+    if (current_user_can('edit_user', $user_id)) {
+
+        update_user_meta($user_id, 'cxr_idrepublicana', $_POST['cxr_idrepublicana']);
+    }
+}
+
+/**
+ * Valida la Identitat Digital Republicana
+ *
+ * @param array $errors
+ * @param array $update
+ * @param User $user
+ *
+ * @return array $errors
+ */
+function validate_cxr_idrepublicana($errors, $update, $user)
+{
+    if (!empty($_POST['cxr_idrepublicana'])) {
+
+        $response = file_get_contents('https://apis.consellrepublica.cat/idserv/validate?idCiutada=' . $_POST['cxr_idrepublicana']);
+
+        $json = json_decode($response, true);
+
+        if ($json['state'] != 'VALID_ACTIVE') {
+
+            $errors->add('cxr_idrepublicana', "<strong>Error</strong>: L'ID Republicana introduïda no és vàlida. Si us plau, torna-ho a intentar.");
+
+        }
+
+        $args = array(
+    		'meta_key'     => 'cxr_idrepublicana',
+    		'meta_value'   => $_POST['cxr_idrepublicana'],
+    		'meta_compare' => '=',
+    		'exclude'      => array($user->ID)
+    	);
+
+    	$user_query = new WP_User_Query($args);
+
+    	$users = $user_query->get_results();
+
+        if ($users) {
+
+            $errors->add('cxr_idrepublicana', "<strong>Error</strong>: L'ID Republicana introduïda ja està en ús. Si us plau, torna-ho a intentar.");
+
+        }
+    }
+
+    return $errors;
+}
+
+/**
+ * Utilitza la Identitat Digital Republicana per a iniciar sessió
+ *
+ * @param array $errors
+ * @param array $update
+ * @param User $user
+ *
+ * @return void
+ */
+function authenticate_cxr_idrepublicana(&$username)
+{
+    $user = get_user_by('login', $username);
+
+    if (!$user) {
+
+        $user = get_user_by('email', $username);
+
+        if (!$user) {
+
+            $args = array(
+        		'meta_key'     => 'cxr_idrepublicana',
+        		'meta_value'   => $username,
+        		'meta_compare' => '=',
+        	);
+
+            $user_query = new WP_User_Query($args);
+
+        	$query_users = $user_query->get_results();
+
+            if (sizeof($query_users) == 1) {
+
+                $username = $query_users[0]->user_login;
+            }
+        }
+    }
+}
+
+?>
